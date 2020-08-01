933c096f5aa042990c440818aaf7a8986f4d13bf
Charles Reid <chmreid@ucsc.edu>
2020-07-31 17:07:55 -0700

    make ALL images medium-zoom images so we can use markdown image syntax

--- docs/javascript/extra.js
+++ docs/javascript/extra.js
@@ -1,2 +1,6 @@
 // Turn any image with class attribute of "zoom-default" to a medium-zoom image
+img = document.getElementsByTagName('img');
+for(i = 0; i < img.length; i++) {
+    img[i].className += " zoom-default";
+}
 const zoomDefault = mediumZoom('.zoom-default')
